d0688ab9503181d799c633057b0dbb6370cb44d6
Emiliano Nataren Del Rivero <eminataren2002@gmail.com>
2024-09-21 14:21:59 -0600

    upload

---
+++ src/message/message.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class MessageService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+  create(createMessageDto: CreateMessageDto) {
+    return this.message.create({
+      data: createMessageDto,
+    });
+  }
+
+  findAll() {
+    return this.message.findMany({ orderBy: { createdAt: 'desc' } });
+  }
+
+  findOne(id: string) {
+    return this.message.findFirst({ where: { id } });
+  }
+}
